aeb3630fb601b4469696692792cf7298985bc6eb
akosivic <vic.salak@gmail.com>
2021-12-20 23:19:26 +0800

    updated the skills

--- src/routes/skills.tsx
+++ src/routes/skills.tsx
@@ -8,7 +8,7 @@ function Skills() {
     };
     let max: number = 10;
 
-    let professionalSkills: Array<skill> = [{ skill: 'C#.net', level: 7 }, { skill: 'Javascript', level: 7 }, { skill: 'Bootstrap', level: 5 }, { skill: 'JQuery', level: 5 }, { skill: 'Unit Testing', level: 5 }, { skill: 'OOP', level: 7 }, { skill: 'Unit Testing', level: 7 }]
+    let professionalSkills: Array<skill> = [{ skill: 'C#.net', level: 7 }, { skill: 'JavaScript', level: 7 }, { skill: 'Bootstrap', level: 5 }, { skill: 'JQuery', level: 5 }, { skill: 'Unit Testing', level: 5 }, { skill: 'OOP', level: 7 }, { skill: 'SQL', level: 7 }, { skill: 'WPF', level: 7 }]
     let interpersonalSkills: Array<skill> = [{ skill: 'People Management', level: 5 }, { skill: 'Scrum Master', level: 7 }]
 
     return (
@@ -17,32 +17,29 @@ function Skills() {
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <h2>Professional</h2>
+                        {/* <h2>Professional</h2> */}
                         {professionalSkills.map((skills, idx) => (
                             <div className="row">
                                 <div className="col-4">
-                                    <span style={{ alignContent: "10px" }}>{skills.skill}</span>
+                                    <span key={idx} style={{ float: "left" }}>{skills.skill}</span>
                                 </div>
                                 <div className="col-8" >
-                                    <ProgressBar className='col-sm-9' label={skills.skill} now={skills.level} max={max} />
+                                    <ProgressBar key={idx} className='col-sm-9' now={skills.level} max={max} />
                                 </div>
                             </div>
                         ))}
                     </div>
                     <div className="col">
-                        <h2>Interpersonal</h2>
+                        {/* <h2>Interpersonal</h2> */}
                         {interpersonalSkills.map((skills, idx) => (
                             <div className="row">
                                 <div className="col-4">
-                                    <span>{skills.skill}</span>
+                                    <span style={{ float: "left" }}>{skills.skill}</span>
                                 </div>
                                 <div className="col-8" >
-                                    <ProgressBar className='col-sm-9' label={skills.skill} now={skills.level} max={max} />
+                                    <ProgressBar className='col-sm-9' now={skills.level} max={max} />
                                 </div>
                             </div>
-                            // <div className="skills">
-
-                            // </div>
                         ))}
                     </div>
                 </div>
